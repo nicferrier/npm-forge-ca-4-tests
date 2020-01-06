576d665cfbba83494ec78e59984778d04b6ce3d9
Nic Ferrier <nic@ferrier.me.uk>
2020-01-06 13:54:12 +0000

    stupid bug fix and bump version

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const cacerts = require("./cacerts");
 const simpleCertService = require("./simple-cert-service.js");
 
-exports.makeCa = makeCa;
-exports.makeCsr = makeCsr;
-exports.pki = pki;
+exports.makeCa = cacerts.makeCa;
+exports.makeCsr = cacerts.makeCsr;
+exports.pki = cacerts.pki;
 exports.simpleCertService = simpleCertService;
 
 // End
